3c0bc8b22f4df9fd0ff978f8d224fbc5badf005b
itom <itom0717@gmail.com>
2025-05-22 13:56:52 +0900

    大幅修正 年月フォルダに移動に変更

--- Program.cs
+++ Program.cs
@@ -2,20 +2,20 @@
 using System.Runtime.InteropServices;
 
 ///-----------------------------------
-///ファイル名変更プログラム
+///ファイル名変更＆フォルダ仕分けプログラム
 ///-----------------------------------
 try
 {
     Console.WriteLine("■■■■■■■■■■■■■■■■■■■■■");
-    Console.WriteLine("ファイル名変更プログラム");
+    Console.WriteLine("ファイル名変更＆フォルダ仕分けプログラム");
     Console.WriteLine("■■■■■■■■■■■■■■■■■■■■■");
     Console.WriteLine("");
 
     //コマンドライン引数を配列で取得する
     string[] cmds = System.Environment.GetCommandLineArgs();
-    if (cmds.Length <= 3)
+    if (cmds.Length <= 4)
     {
-        throw new Exception("引数が足りません。\n[対象パス] [ワイルドカード] [ファイルプレフィックス] ([保存パス] [フォルダモード])");
+        throw new Exception("引数が足りません。\n[対象パス] [対象ファイルのワイルドカード] [変更後のファイルプレフィックス] [別フォルダに移動させる日数]");
     }
 
     // 対象パスの取得
@@ -26,38 +26,25 @@ try
         throw new Exception($"対象パスが存在しません。");
     }
 
-    //ワイルドカード
+    //対象ファイルのワイルドカード
     string wildCard = cmds[2];
-    if(string.IsNullOrEmpty(wildCard))
+    if (string.IsNullOrEmpty(wildCard))
     {
         wildCard = "*.*";
     }
     Console.WriteLine($"ワイルドカード :{wildCard} ");
 
+
     //ファイル名の前半部分
     string pefix = cmds[3];
     Console.WriteLine($"プレフィックス :{pefix} ");
-
-    //フォルダ移動モード指定
-    // 0 : 移動なし
-    // 1 : 年月日単位のフォルダに移動
-    int mode = 0;
-    string savePath = "";
-    if(cmds.Length >= 5)
-    {
-        string m = cmds[5];
-        int.TryParse(m, out mode);
-        Console.WriteLine($"フォルダ移動モード :{mode} ");
-
-        savePath = cmds[4];
-        Console.WriteLine($"保存パス :{savePath} ");
-        if (!System.IO.Directory.Exists(savePath))
-        {
-            throw new Exception($"保存パスが存在しません。");
-        }
-    }
+    Console.WriteLine("");
 
 
+    //移動させない日数
+    int notMovedays = 0;
+    int.TryParse(cmds[4], out notMovedays);
+    Console.WriteLine($"移動させない日数 :{notMovedays.ToString()} ");
     Console.WriteLine("");
 
 
@@ -65,11 +52,11 @@ try
     string[] files = Directory.GetFiles(tgtPath, wildCard, SearchOption.TopDirectoryOnly);
     foreach (string file in files)
     {
-
         string filename = Path.GetFileName(file);
         string? filePath = Path.GetDirectoryName(file);
-        if(filePath == null || string.IsNullOrWhiteSpace(filename))
+        if (filePath == null || string.IsNullOrWhiteSpace(filename))
         {
+            //あり得ないが処理しない
             continue;
         }
 
@@ -78,15 +65,17 @@ try
 
         string newFilename;
         string newFilePath;
-        do
+        do//同名が存在する場合の対策（存在しなくなるまでループ）
         {
             //新しいファイル名を生成
             newFilename = $"{pefix}_{fileTime.ToString("yyyyMMdd_HHmmss")}{Path.GetExtension(file)}";
 
-            //直近数日間はサブフォルダに移動させない
-            DateTime checkTime = DateTime.Now.AddDays(mode * -1);
-             if (mode <= 0 || checkTime <=fileTime)
+            //指定日数以上はサブフォルダに移動させる
+            DateTime checkTime = DateTime.Now.AddDays(notMovedays * -1);
+            if (notMovedays <= 0 || checkTime <= fileTime)
             {
+                //移動させない
+
                 //同一階層
                 newFilePath = Path.Combine(filePath, newFilename);
 
@@ -101,45 +90,29 @@ try
             {
                 //保存先
                 string savePathYear = fileTime.ToString("yyyy年");
-                string savePathMonth = fileTime.ToString("MM月");
-                string savePathday = fileTime.ToString("dd日");
+                string savePathMonth = fileTime.ToString("yyyy年MM月");
+                string savePathday = fileTime.ToString("yyyy年MM月dd日");
 
                 //年のフォルダ作成
-                string saveFillPath = savePath;
+                string saveFillPath = tgtPath;
                 saveFillPath = Path.Combine(saveFillPath, savePathYear);
                 if (!Directory.Exists(saveFillPath))
                 {
                     Directory.CreateDirectory(saveFillPath);
                 }
 
-                ////月のフォルダ作成
-                //saveFillPath = Path.Combine(saveFillPath, savePathMonth);
-                //if (!Directory.Exists(saveFillPath))
-                //{
-                //    Directory.CreateDirectory(saveFillPath);
-                //}
-
-                ////日のフォルダ作成
-                //saveFillPath = Path.Combine(saveFillPath, savePathday);
-                //if (!Directory.Exists(saveFillPath))
-                //{
-                //    Directory.CreateDirectory(saveFillPath);
-                //}
-
-                //月日のフォルダ作成
-                saveFillPath = Path.Combine(saveFillPath, savePathMonth+savePathday);
+                //月のフォルダ作成
+                saveFillPath = Path.Combine(saveFillPath, savePathMonth);
                 if (!Directory.Exists(saveFillPath))
                 {
                     Directory.CreateDirectory(saveFillPath);
                 }
 
-
+                //移動先パス＆新しいファイル名
                 newFilePath = Path.Combine(saveFillPath, newFilename);
             }
 
-
-
-            //同名が存在するかチェック
+            //同名が存在するかチェックし、なければ名前変更及びフォルダ移動
             if (!File.Exists(newFilePath))
             {
                 //ファイル名を変更
@@ -148,6 +121,7 @@ try
                 break;
             }
 
+            //存在するので、1秒進めたファイル名する
             fileTime = fileTime.AddSeconds(1);
 
         } while (true);
